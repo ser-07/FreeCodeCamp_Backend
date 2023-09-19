18a199471aad82c9dceb1361a45f9f014476b5c9
ser-07 <ser23887@gmail.com>
2023-09-19 23:03:37 +0530

    #3- sendFile method

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -4,9 +4,19 @@ let app = express();
 
 console.log("Hello World");
 
+/*
 app.get('/', (req, res)=>{
     res.status(200);
     res.send("Hello Express");
+});
+*/
+
+//#3
+app.get('/', (req, res)=>{
+    let absolutePath = __dirname + '/views/index.html'
+    console.log(__dirname, absolutePath);
+    res.status(200);
+    res.sendFile(absolutePath);
 });
 
 
