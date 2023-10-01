ef223d41a52c97ce9124280886c50190da186af6
ser-07 <ser23887@gmail.com>
2023-10-01 21:38:28 +0530

    #6 Chain Middleware

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -53,6 +53,14 @@ app.get('/json' , (req, res ) => {
 });
 
 
+app.get('/now', function(req, res, next) {
+    req.time = new Date().toString(); 
+    next();
+  }, function(req, res) {
+    res.json({"time": req.time});
+  });
+
+
 
 
 
