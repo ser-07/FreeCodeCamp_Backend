0943c509935f4ba228eec68e6212e71c8edf42d0
ser-07 <ser23887@gmail.com>
2023-10-10 22:30:59 +0530

    Completed all requirements-Ready to test

--- Timestamp_microservice_FCC/index.js
+++ Timestamp_microservice_FCC/index.js
@@ -24,6 +24,34 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'Hello from Sreejith!'});
 });
 
+app.get('/api/', (req,res)=> {
+  console.log("Jere");
+  const date1 = new Date();
+  res.json({"unix":date1.valueOf(),"utc":date1.toUTCString()});
+})
+
+app.get('/api/:dateInput', (req, res)=>{
+  console.log("Here");
+  let reg = /^\d+$/;
+  let incorrectFormatFlg = reg.test(req.params.dateInput);
+
+  const datefromDate = new Date(req.params.dateInput);
+  const dateFromUnixTimestamp = Date(Number(req.params.dateInput));
+  
+  // console.log(req.params.dateInput,incorrectFormatFlg, datefromDate, dateFromUnixTimestamp);
+  // console.log(typeof(req.params.dateInput), req.params.dateInput, date1);
+  // console.log(date1 == 'Invalid Date');
+  if(datefromDate != 'Invalid Date'){
+    return res.status(200).json({"unix":datefromDate.valueOf(),"utc":datefromDate.toUTCString()});
+  }
+  else if(incorrectFormatFlg && dateFromUnixTimestamp){
+    return res.status(200).json({"unix":req.params.dateInput,"utc":dateFromUnixTimestamp});
+     
+  }
+  res.json({ error : "Invalid Date" })
+
+})
+
 // listen for requests :)
 var listener = app.listen(5000, //process.env.PORT,
  function () {
